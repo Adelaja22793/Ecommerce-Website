2550038b6500d3f0fffe9ea59b3a1dc5c392a784
wilson olabode <wilson olabode@WILSON>
2020-01-24 15:55:37 +0100

    added main category to admin

---
+++ Ecommerce Website/Pages/Admin/MainCategory/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ecommerce_Website.Data;
+
+namespace Ecommerce_Website.Pages.Admin.MainCategory
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Ecommerce_Website.Data.ApplicationDbContext _context;
+
+        public DetailsModel(Ecommerce_Website.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Data.MainCategory MainCategory { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            MainCategory = await _context.MainCategories.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (MainCategory == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
---
+++ Ecommerce Website/Pages/Admin/MainCategory/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Ecommerce_Website.Data;
+
+namespace Ecommerce_Website.Pages.Admin.MainCategory
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Ecommerce_Website.Data.ApplicationDbContext _context;
+
+        public IndexModel(Ecommerce_Website.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Data.MainCategory> MainCategory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            MainCategory = await _context.MainCategories.ToListAsync();
+        }
+    }
+}
