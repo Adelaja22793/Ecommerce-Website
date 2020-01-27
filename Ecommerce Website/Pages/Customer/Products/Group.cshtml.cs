5b0f6ca1d891ada2038c63f0aab952c4baf35afe
wilson olabode <wilson olabode@WILSON>
2020-01-27 17:14:11 +0100

    added routing to group selected from product

---
+++ Ecommerce Website/Pages/Customer/Products/Group.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce_Website.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce_Website.Pages.Customer.Products
+{
+    public class GroupModel : PageModel
+    {
+        private readonly Ecommerce_Website.Data.ApplicationDbContext _context;
+
+        public SubCategory SubCategory { get; set; }
+
+        public GroupModel(Ecommerce_Website.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnGetAsync(int subgroupId)
+        {
+            //get the sub category that has the id
+            SubCategory = await _context.SubCategories.FindAsync(subgroupId);
+        }
+    }
+}
