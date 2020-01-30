be64b91f0c2d5c993beace859bd7685205cfa227
wilson olabode <wilson olabode@WILSON>
2020-01-30 16:12:51 +0100

    implemented customer adding to cart

--- Ecommerce Website/Pages/Customer/Products/Group.cshtml.cs
+++ Ecommerce Website/Pages/Customer/Products/Group.cshtml.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Ecommerce_Website.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc; 
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecommerce_Website.Pages.Customer.Products
 {
+    // ensure that only a logged in user can access this page Authorize(Roles =the role)
+    [Authorize(Roles = "Customer,Admin")]
     public class GroupModel : PageModel
     {
         private readonly Ecommerce_Website.Data.ApplicationDbContext _context;
 
+        private readonly UserManager<IdentityUser> _userManager;
+
         public SubCategory SubCategory { get; set; }
 
-        public GroupModel(Ecommerce_Website.Data.ApplicationDbContext context)
-        { 
+        
+
+        public GroupModel(Ecommerce_Website.Data.ApplicationDbContext context,
+            UserManager<IdentityUser> userManager)
+         {
+            _userManager = userManager;
             _context = context;
         }
 
@@ -31,6 +41,7 @@ namespace Ecommerce_Website.Pages.Customer.Products
                     .ThenInclude(l => l.Images)
                     .FirstOrDefaultAsync(n => n.Id == subgroupId.Value);
                 return Page();
+               
             }
             else
             {
@@ -39,5 +50,35 @@ namespace Ecommerce_Website.Pages.Customer.Products
             }
            
         }
+
+
+
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+            if(product != null)
+            {
+                var cart = new Cart();
+                cart.ProductId = product.Id;
+                cart.Quantity = 1;
+                cart.CartState = Enumerations.CartState.InCart;
+
+
+
+                //_context.Carts.Add(cart);
+            }
+
+            return Page();
+        }
+
+
+
+        private Task<IdentityUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
     }
+    
+
+
+  
 }
